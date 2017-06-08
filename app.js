d7a5327f499175e6ed2467c13e268fe343dd28a3
Joe Kelley <joe@email.com>
2017-06-07 20:40:03 -0700

    got rid of jerrys bad comment

--- app.js
+++ app.js
@@ -18,8 +18,6 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 
-// Really bad comment
-/ JERRY TYPED THIS COMMENT because his boss told him too!
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
