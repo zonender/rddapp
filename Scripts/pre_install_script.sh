507bd12b35a2b09ed2a3692cf1af7ad29e5910e9
Joe Kelley <joe@email.com>
2017-06-07 19:46:09 -0700

    got rid of everything

--- Scripts/pre_install_script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo 'Starting Pre-Install Script'
-apt-get install nodejs-legacy
-apt-get install npm
-npm install -g pm2
-
-pm2 stop all
-echo 'Pre-Install Script Finished'
--- Scripts/start_script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo 'Starting Start Script'
-pm2 start /bin/www --name RubberDuckyDashboard
-echo 'Start Script Finished!'
