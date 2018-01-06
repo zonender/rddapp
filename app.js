c20fe4dc24d187f50a326b15c53a272aa66401f1
Asim Abdelgadir <acsaasim@hotmail.com>
2018-01-05 22:18:33 -0500

    1st commit

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.set('view engine', 'ejs');
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 
 // App Init functions
-app.use(logger('dev');
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
